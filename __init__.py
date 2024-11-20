4b78f0844fd007c506dde08592c083a51e6c84c8
martias971 <matthieupanoma@gmail.com>
2024-11-20 15:39:00 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,9 +11,6 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html') #Comm2
 
-@app.route("https://martias971.alwaysdata.net/TP/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
   
 if __name__ == "__main__":
   app.run(debug=True)
